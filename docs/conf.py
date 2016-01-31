35ee5bf25f91ab5ab2c567ddcbef33fba51c76c5
Brett <Brett.Jackson@hscic.gov.uk>
2016-01-31 06:42:08 +0000

    test

--- docs/conf.py
+++ docs/conf.py
@@ -108,13 +108,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    html_theme = 'core'
-else:
-    html_theme = 'nature'
-
-#html_theme = 'nature'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
